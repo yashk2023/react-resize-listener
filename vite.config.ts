cbe6eca7894900fc8172d8cf558e1e95393a4a33
yashk2023 <124333962+yashk2023@users.noreply.github.com>
2025-01-16 14:05:19 +0530

    - Changes on vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: './src/main.tsx', // Path to your library's entry point
+      name: 'react-resize-listener-hook',  // Global variable name (useful for UMD builds)
+      fileName: (format) => `react-resize-listener-hook.${format}.js`,
+    },
+    rollupOptions: {
+      // Externalize dependencies that should not be bundled
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
 })
